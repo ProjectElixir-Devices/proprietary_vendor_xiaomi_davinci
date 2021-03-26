e9277bc2597cbcdb117ef50ec0f6e4015469ef79
ArixElo <patroxgamer@gmail.com>
2021-03-26 10:52:56 +0000

    davinci: Add old custom popup sounds
    davinci: Add JoJo popup sounds
    
    davinci: Add SuperMario popup sounds
    
    davinci: Add star wars popup sounds
    
    davinci: Add Anime popup sounds
    Co-Authored-By: Raaj52 <19911565+Raaj52@users.noreply.github.com>
    Co-Authored-By: Arian <39061882+ArianK16a@users.noreply.github.com>
    Co-Authored-By: wannacry <57298636+rerererererererererer@users.noreply.github.com>

--- davinci-vendor.mk
+++ davinci-vendor.mk
@@ -18,6 +18,18 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_muqin_up.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_muqin_up.ogg \
     vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_yingyan_down.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_yingyan_down.ogg \
     vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_yingyan_up.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_yingyan_up.ogg \
+    vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_jojo_down.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_jojo_down.ogg \
+    vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_jojo_up.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_jojo_up.ogg \
+    vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_super_mario_down.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_super_mario_down.ogg \
+    vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_super_mario_up.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_super_mario_up.ogg \
+    vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_imperial_march_down.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_imperial_march_down.ogg \
+    vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_imperial_march_up.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_imperial_march_up.ogg \
+    vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_lightsaber_down.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_lightsaber_down.ogg \
+    vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_lightsaber_up.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_lightsaber_up.ogg \
+    vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_throne_room_down.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_throne_room_down.ogg \
+    vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_throne_room_up.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_throne_room_up.ogg \
+    vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_anime_moan_down.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_anime_moan_down.ogg \
+    vendor/xiaomi/davinci/proprietary/media/audio/ui/popup_anime_moan_up.ogg:$(TARGET_COPY_OUT_SYSTEM)/media/audio/ui/popup_anime_moan_up.ogg \
     vendor/xiaomi/davinci/proprietary/vendor/etc/acdbdata/Forte/Forte_Bluetooth_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/Forte/Forte_Bluetooth_cal.acdb \
     vendor/xiaomi/davinci/proprietary/vendor/etc/acdbdata/Forte/Forte_Codec_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/Forte/Forte_Codec_cal.acdb \
     vendor/xiaomi/davinci/proprietary/vendor/etc/acdbdata/Forte/Forte_General_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/Forte/Forte_General_cal.acdb \
