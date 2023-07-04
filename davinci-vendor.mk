21057a4f04d0077e3bb326ea35453d03e7acd382
basamaryan <basam.aryan@gmail.com>
2023-07-04 18:05:12 +0200

    davinci: Bring back face detection libs
    Change-Id: I4a554e6f6405b989359bb831e61dac6d0efd5a4c

--- davinci-vendor.mk
+++ davinci-vendor.mk
@@ -45,6 +45,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/davinci/proprietary/vendor/etc/camera/eyelineblush.cng:$(TARGET_COPY_OUT_VENDOR)/etc/camera/eyelineblush.cng \
     vendor/xiaomi/davinci/proprietary/vendor/etc/camera/golden_rear_dc_cal_wu.bin:$(TARGET_COPY_OUT_VENDOR)/etc/camera/golden_rear_dc_cal_wu.bin \
     vendor/xiaomi/davinci/proprietary/vendor/etc/camera/lf_facerank_model.bin:$(TARGET_COPY_OUT_VENDOR)/etc/camera/lf_facerank_model.bin \
+    vendor/xiaomi/davinci/proprietary/vendor/etc/camera/facedetectpp_0_5_2_model:$(TARGET_COPY_OUT_VENDOR)/etc/camera/facedetectpp_0_5_2_model \
     vendor/xiaomi/davinci/proprietary/vendor/etc/camera/mibokeh_712_opencl.bin:$(TARGET_COPY_OUT_VENDOR)/etc/camera/mibokeh_712_opencl.bin \
     vendor/xiaomi/davinci/proprietary/vendor/etc/camera/mibokeh_712_parameter.bin:$(TARGET_COPY_OUT_VENDOR)/etc/camera/mibokeh_712_parameter.bin \
     vendor/xiaomi/davinci/proprietary/vendor/etc/camera/sdm_ys_32p_120_21_5_perturb50.bin:$(TARGET_COPY_OUT_VENDOR)/etc/camera/sdm_ys_32p_120_21_5_perturb50.bin \
@@ -225,6 +226,7 @@ PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.motor@1.0-impl \
     libFaceGrade \
     libHalSuperSensorServer \
+    libFaceDetectpp-0.5.2 \
     libSuperSensor \
     libSuperSensorCPU \
     libVDClearShot \
@@ -261,6 +263,7 @@ PRODUCT_PACKAGES += \
     libcom.qti.chinodeutils \
     libft2vendor \
     libgf_hal \
+    libfacedet \
     libmialgo_fs \
     libmialgo_rfs \
     libmialgo_sd \
